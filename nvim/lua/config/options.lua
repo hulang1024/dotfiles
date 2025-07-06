8a73b97e6d9a2a3fe15bee8e546f68df4f4e4cbb
hulang <1013644379@qq.com>
2025-07-06 21:04:50 +0800

    Update

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,28 +1,42 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Options are automatically loaded before lazy.nstartup
+-- Default options that are always set: https://github.com/LazyLazyblob/main/lua/lazyconfig/options.lua
 -- Add any additional options here
-vim.g.mapleader = " "
-vim.opt.encoding = "utf-8"
-vim.opt.number = true
+local opt = vim.opt
+local g = vim.g
+
+g.mapleader = " "
+
+opt.encoding = "utf-8"
+opt.number = true
+opt.relativenumber = true
+opt.signcolumn = "yes"
+
 -- 在窗口标题栏显示当前文件名
-vim.opt.backup = false
-vim.opt.title = true
-vim.opt.visualbell = true
-vim.opt.showcmd = true
-vim.opt.cmdheight = 1
-vim.opt.hlsearch = true
+opt.backup = false
+opt.title = true
+opt.visualbell = true
+opt.clipboard = "unnamedplus"
+
+opt.showcmd = true
+opt.cmdheight = 1
+
+opt.hlsearch = true
+opt.smartcase = true
+opt.ignorecase = true
+
+opt.splitbelow = true
+opt.splitright = true
+
 -- 禁止在屏幕宽度不够时自动换行
-vim.opt.wrap = false
-vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+opt.wrap = false
+opt.expandtab = true
+opt.tabstop = 2
+opt.shiftwidth = 2
 -- 新行自动继承上行的缩进
-vim.opt.autoindent = true
+opt.autoindent = true
 -- 根据语法规则智能调整缩进
-vim.opt.smartindent = true
+opt.smartindent = true
 -- 在行首按 Tab 时插入 shiftwidth 的空格，其他地方按 tabstop 规则
-vim.opt.smarttab = true
+opt.smarttab = true
 -- 当一行文字被折行显示时，续行保持与首行相同的缩进
-vim.opt.breakindent = true
-vim.opt.mouse = ""
-vim.opt.shell = "powershell"
+opt.breakindent = true
